c180f932942653e0cd7fcd432343563eceedb9e6
oliver-giersch <oliver.giersch@b-tu.de>
2020-12-20 16:54:35 +0100

    increases time for LSCQ benchmarks

--- slurm/micro/scq2.sh
+++ slurm/micro/scq2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #SBATCH --job-name=scq2_micro
-#SBATCH --time 01:00:00
+#SBATCH --time 01:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --partition=standard96:test
+#SBATCH --partition=standard96
 #SBATCH -L ansys:1
 
 sh ./micro/run_pairs_and_bursts.sh scq2 10M 100
