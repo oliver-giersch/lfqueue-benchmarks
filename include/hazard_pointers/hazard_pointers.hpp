8950b5bbe8352f680a805c72bf006f507150f4d3
Oliver Giersch <oliver.giersch@b-tu.de>
2026-04-27 10:59:24 +0200

    update some things

--- include/hazard_pointers/hazard_pointers.hpp
+++ include/hazard_pointers/hazard_pointers.hpp
@@ -5,120 +5,139 @@
 
 #include <stdexcept>
 
-namespace memory {
-template<typename T>
-hazard_pointers<T>::hazard_pointers(
-    std::size_t num_threads,
-    std::size_t num_hazard_pointers,
-    std::size_t scan_threshold
-) : m_num_hazard_pointers{ num_hazard_pointers },
-    m_scan_threshold{ scan_threshold },
-    m_thread_blocks{ num_threads }
+namespace memory
 {
-  if (num_hazard_pointers > MAX_HAZARD_POINTERS) {
-    throw std::invalid_argument("`num_hazard_pointers` must be <= 8");
-  }
-
-  for (auto& thread_block : this->m_thread_blocks) {
-    thread_block.retired_objects.reserve(scan_threshold);
-  }
-}
-
-template<typename T>
-hazard_pointers<T>::~hazard_pointers<T>() noexcept {
-  for (auto& thread_block : this->m_thread_blocks) {
-    for (auto retired : thread_block.retired_objects) {
-      delete retired;
-    }
-  }
-}
-
-template<typename T>
-void hazard_pointers<T>::clear_one(std::size_t thread_id, std::size_t hp) {
-  auto& thread_block = this->m_thread_blocks[thread_id];
-  thread_block.hazard_ptrs[hp].ptr.store(nullptr, std::memory_order_release);
-}
-
-template<typename T>
-void hazard_pointers<T>::clear(std::size_t thread_id) {
-  auto& thread_block = this->m_thread_blocks[thread_id];
-  for (auto& hazard_ptr : thread_block.hazard_ptrs) {
-    hazard_ptr.ptr.store(nullptr, std::memory_order_relaxed);
-  }
-
-  std::atomic_thread_fence(std::memory_order_release);
-}
-
-template<typename T>
-typename hazard_pointers<T>::pointer hazard_pointers<T>::protect(
-    const std::atomic<hazard_pointers::pointer>& atomic,
-    std::size_t thread_id,
-    std::size_t hp
-) {
-  auto& hazard_ptr = this->m_thread_blocks[thread_id].hazard_ptrs[hp];
-  auto curr = atomic.load(std::memory_order_relaxed);
-  while (true) {
-    hazard_ptr.ptr.store(curr, std::memory_order_seq_cst);
-    const auto temp = atomic.load(std::memory_order_acquire);
-    if (curr == temp) {
-      return curr;
+  template <typename T>
+  hazard_pointers<T>::hazard_pointers(
+      std::size_t num_threads,
+      std::size_t num_hazard_pointers,
+      std::size_t scan_threshold) : m_num_hazard_pointers{num_hazard_pointers},
+                                    m_scan_threshold{scan_threshold},
+                                    m_thread_blocks{num_threads}
+  {
+    if (num_hazard_pointers > MAX_HAZARD_POINTERS)
+    {
+      throw std::invalid_argument("`num_hazard_pointers` must be <= 8");
     }
 
-    curr = temp;
-  }
-}
-
-template<typename T>
-typename hazard_pointers<T>::pointer hazard_pointers<T>::protect_ptr(
-    hazard_pointers::pointer ptr,
-    std::size_t thread_id,
-    std::size_t hp
-) {
-  auto& hazard_ptr = this->m_thread_blocks[thread_id].hazard_ptrs[hp];
-  hazard_ptr.ptr.store(ptr, std::memory_order_seq_cst);
-
-  return ptr;
-}
-
-template<typename T>
-void hazard_pointers<T>::retire(hazard_pointers::pointer ptr, std::size_t thread_id) {
-  auto& thread_retired_objects = this->m_thread_blocks[thread_id].retired_objects;
-  thread_retired_objects.push_back(ptr);
-
-  if (thread_retired_objects.size() < this->m_scan_threshold) {
-    return;
-  }
-
-  std::size_t curr = 0;
-  while (curr < thread_retired_objects.size()) {
-    const auto retired = thread_retired_objects[curr];
-    if (this->can_reclaim(retired)) {
-      auto back = thread_retired_objects.back();
-      if (retired != back) {
-        std::swap(thread_retired_objects[curr], back);
-      }
-
-      thread_retired_objects.pop_back();
-      delete retired;
-      continue;
+    for (auto &thread_block : this->m_thread_blocks)
+    {
+      thread_block.retired_objects.reserve(scan_threshold);
     }
-
-    ++curr;
   }
-}
 
-template <typename T>
-bool hazard_pointers<T>::can_reclaim(pointer retired) const {
-  for (const auto& thread_block : this->m_thread_blocks) {
-    for (auto hp = 0; hp < this->m_num_hazard_pointers; ++hp) {
-      if (thread_block.hazard_ptrs[hp].ptr.load() == retired) {
-        return false;
+  template <typename T>
+  hazard_pointers<T>::~hazard_pointers<T>() noexcept
+  {
+    for (auto &thread_block : this->m_thread_blocks)
+    {
+      for (auto retired : thread_block.retired_objects)
+      {
+        delete retired;
       }
     }
   }
 
-  return true;
-}
+  template <typename T>
+  void hazard_pointers<T>::clear_one(std::size_t thread_id, std::size_t hp)
+  {
+    auto &thread_block = this->m_thread_blocks[thread_id];
+    thread_block.hazard_ptrs[hp].ptr.store(nullptr, release);
+  }
+
+  template <typename T>
+  void hazard_pointers<T>::clear(std::size_t thread_id)
+  {
+    auto &thread_block = this->m_thread_blocks[thread_id];
+    for (auto &hazard_ptr : thread_block.hazard_ptrs)
+    {
+      hazard_ptr.ptr.store(nullptr, relaxed);
+    }
+
+    std::atomic_thread_fence(release);
+  }
+
+  template <typename T>
+  hazard_pointers<T>::pointer hazard_pointers<T>::protect(
+      const std::atomic<hazard_pointers::pointer> &atomic,
+      std::size_t thread_id,
+      std::size_t hp)
+  {
+    auto &hazard_ptr = this->m_thread_blocks[thread_id].hazard_ptrs[hp];
+    auto curr = atomic.load(relaxed);
+    while (true)
+    {
+      hazard_ptr.ptr.store(curr, seq_cst);
+      const auto temp = atomic.load(acquire);
+      if (curr == temp)
+      {
+        return curr;
+      }
+
+      curr = temp;
+    }
+  }
+
+  template <typename T>
+  hazard_pointers<T>::pointer hazard_pointers<T>::protect_ptr(
+      hazard_pointers::pointer ptr,
+      std::size_t thread_id,
+      std::size_t hp)
+  {
+    auto &hazard_ptr = this->m_thread_blocks[thread_id].hazard_ptrs[hp];
+    hazard_ptr.ptr.store(ptr, seq_cst);
+
+    return ptr;
+  }
+
+  template <typename T>
+  void hazard_pointers<T>::retire(hazard_pointers::pointer ptr, std::size_t thread_id)
+  {
+    auto &thread_retired_objects = this->m_thread_blocks[thread_id].retired_objects;
+    thread_retired_objects.push_back(ptr);
+
+    if (thread_retired_objects.size() < this->m_scan_threshold)
+    {
+      return;
+    }
+
+    std::size_t curr = 0;
+    while (curr < thread_retired_objects.size())
+    {
+      const auto retired = thread_retired_objects[curr];
+      if (this->can_reclaim(retired))
+      {
+        auto back = thread_retired_objects.back();
+        if (retired != back)
+        {
+          std::swap(thread_retired_objects[curr], back);
+        }
+
+        thread_retired_objects.pop_back();
+        delete retired;
+        continue;
+      }
+
+      ++curr;
+    }
+  }
+
+  template <typename T>
+  bool hazard_pointers<T>::can_reclaim(pointer retired) const
+  {
+    for (const auto &thread_block : this->m_thread_blocks)
+    {
+      for (auto hp = 0; hp < this->m_num_hazard_pointers; ++hp)
+      {
+        if (thread_block.hazard_ptrs[hp].ptr.load() == retired)
+        {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
 }
 
 #endif /* LOO_QUEUE_BENCHES_HAZARD_POINTERS_HPP */
--- include/queues/msc/michael_scott.hpp
+++ include/queues/msc/michael_scott.hpp
@@ -3,88 +3,134 @@
 
 #include "michael_scott_fwd.hpp"
 
-namespace msc {
-template <typename T>
-queue<T>::queue(std::size_t max_threads) :
-  m_hazard_pointers{ max_threads, 2, 100 }
+namespace msc
 {
-  const auto sentinel = new node_t{ nullptr };
-  this->m_head.store(sentinel, relaxed);
-  this->m_tail.store(sentinel, relaxed);
-}
-
-template <typename T>
-queue<T>::~queue<T>() noexcept {
-  auto curr = this->m_head.load(relaxed);
-  while (curr != nullptr) {
-    auto next = curr->next.load(relaxed);
-    delete curr;
-    curr = next;
-  }
-}
-
-template <typename T>
-void queue<T>::enqueue(queue::pointer elem, std::size_t thread_id) {
-  if (elem == nullptr) {
-    throw std::invalid_argument("enqueue element must not be nullptr");
+  template <typename T>
+  queue<T>::queue(std::size_t max_threads) : m_hazard_pointers{max_threads, 2, 100}
+  {
+    const auto sentinel = new node_t{nullptr};
+    this->m_head.store(sentinel, relaxed);
+    this->m_tail.store(sentinel, relaxed);
   }
 
-  auto node = new node_t{ elem };
-  while (true) {
-    auto tail = this->m_hazard_pointers.protect_ptr(
-        this->m_tail.load(relaxed), thread_id, HP_ENQ_TAIL
-    );
-
-    if (this->m_tail.load(acquire) != tail) continue;
-
-    node_t* expected = nullptr;
-    if (tail->cas_next(expected, node, release)) {
-      this->cas_tail(tail, node, release);
-      break;
-    } else {
-      this->cas_tail(tail, expected, release);
+  template <typename T>
+  queue<T>::~queue<T>() noexcept
+  {
+    auto curr = this->m_head.load(relaxed);
+    while (curr != nullptr)
+    {
+      auto next = curr->next.load(relaxed);
+      delete curr;
+      curr = next;
     }
   }
 
-  this->m_hazard_pointers.clear_one(thread_id, HP_ENQ_TAIL);
-}
+  /* basically the original algorithm */
+  /*template <typename T>
+  void queue<T>::enqueue(queue::pointer elem, std::size_t thread_id)
+  {
+    auto node = new node_t{elem};
+    while (true)
+    {
+      auto tail = this->m_hazard_pointers.protect_ptr(
+          this->m_tail.load(relaxed), thread_id, HP_ENQ_TAIL);
+      if (this->m_tail.load(relaxed) != tail)
+        continue;
 
-template <typename T>
-typename queue<T>::pointer queue<T>::dequeue(std::size_t thread_id) {
-  auto head = this->m_hazard_pointers.protect(this->m_head, thread_id, HP_DEQ_HEAD);
+      auto next = tail->next.load(relaxed);
 
-  while (head != this->m_tail.load(acquire)) {
-    auto next = this->m_hazard_pointers.protect(head->next, thread_id, HP_DEQ_NEXT);
-    if (this->cas_head(head, next, acquire)) {
-      auto res = next->elem;
-      this->m_hazard_pointers.clear(thread_id);
-      this->m_hazard_pointers.retire(head, thread_id);
+      if (tail != this->m_tail.load(relaxed))
+        continue;
 
-      return res;
+      if (next == nullptr)
+      {
+        if (tail->cas_next(next, node, release))
+        {
+          this->cas_tail(tail, node, release);
+          this->m_hazard_pointers.clear_one(thread_id, HP_ENQ_TAIL);
+          return;
+        }
+      }
+      else
+      {
+        this->cas_tail(tail, next, release);
+      }
     }
-    head = this->m_hazard_pointers.protect(this->m_head, thread_id, HP_DEQ_HEAD);
+  }*/
+
+  template <typename T>
+  void queue<T>::enqueue(queue::pointer elem, std::size_t thread_id)
+  {
+    if (elem == nullptr)
+    {
+      throw std::invalid_argument("enqueue element must not be nullptr");
+    }
+
+    auto node = new node_t{elem};
+    while (true)
+    {
+      auto tail = this->m_hazard_pointers.protect_ptr(
+          this->m_tail.load(relaxed), thread_id, HP_ENQ_TAIL);
+
+      if (this->m_tail.load(relaxed) != tail)
+        continue;
+
+      node_t *expected = nullptr;
+      if (tail->cas_next(expected, node, release))
+      {
+        this->cas_tail(tail, node, release);
+        break;
+      }
+      else
+      {
+        this->cas_tail(tail, expected, release);
+      }
+    }
+
+    this->m_hazard_pointers.clear_one(thread_id, HP_ENQ_TAIL);
   }
 
-  this->m_hazard_pointers.clear(thread_id);
-  return nullptr;
-}
+  template <typename T>
+  typename queue<T>::pointer queue<T>::dequeue(std::size_t thread_id)
+  {
+    auto head = this->m_hazard_pointers.protect(this->m_head, thread_id, HP_DEQ_HEAD);
 
-template <typename T>
-bool queue<T>::cas_head(queue::node_t* curr, queue::node_t* next, std::memory_order order) {
-  return this->m_head.compare_exchange_strong(curr, next, order, relaxed);
-}
+    while (head != this->m_tail.load(relaxed))
+    {
+      auto next = this->m_hazard_pointers.protect(head->next, thread_id, HP_DEQ_NEXT);
+      if (this->cas_head(head, next, relaxed))
+      {
+        auto res = next->elem;
+        this->m_hazard_pointers.clear(thread_id);
+        this->m_hazard_pointers.retire(head, thread_id);
 
-template <typename T>
-bool queue<T>::cas_tail(queue::node_t* curr, queue::node_t* next, std::memory_order order) {
-  return this->m_tail.compare_exchange_strong(curr, next, order, relaxed);
-}
+        return res;
+      }
+      head = this->m_hazard_pointers.protect(this->m_head, thread_id, HP_DEQ_HEAD);
+    }
 
-template <typename T>
-bool queue<T>::node_t::cas_next(
-    queue::node_t*& curr, queue::node_t* next_node, std::memory_order order
-) {
-  return this->next.compare_exchange_strong(curr, next_node, order, relaxed);
-}
+    this->m_hazard_pointers.clear(thread_id);
+    return nullptr;
+  }
+
+  template <typename T>
+  bool queue<T>::cas_head(queue::node_t *curr, queue::node_t *next, std::memory_order order)
+  {
+    return this->m_head.compare_exchange_strong(curr, next, order, relaxed);
+  }
+
+  template <typename T>
+  bool queue<T>::cas_tail(queue::node_t *curr, queue::node_t *next, std::memory_order order)
+  {
+    return this->m_tail.compare_exchange_strong(curr, next, order, relaxed);
+  }
+
+  template <typename T>
+  bool queue<T>::node_t::cas_next(
+      queue::node_t *&curr, queue::node_t *next_node, std::memory_order order)
+  {
+    return this->next.compare_exchange_strong(curr, next_node, order, relaxed);
+  }
 }
 
 #endif /* LOO_QUEUE_BENCHMARK_MICHAEL_SCOTT_HPP */
